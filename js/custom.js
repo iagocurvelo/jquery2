6f163f4c8481df1bdcc93c1db621d531cd99a13a
Iago Curvelo <iagocurvelo@gmail.com>
2023-03-28 12:00:07 -0300

    Primeiro commit

--- js/custom.js
+++ js/custom.js
@@ -48,15 +48,136 @@ $(document).ready(function(){
         alert('Produto esgotado');
 
      })
-     
-     
-     
-     
+
+     /*
+     |* Ouvinte de eventos .nav-modal-open
+     |*/ 
+
+   $('.nav-modal-open').on('click', function(e){
+      
+      e.preventDefault();
+
+      let elem = $(this).attr('rel')
+
+      $('.modal-body').html($('#'+elem).html())
+      
+      $('.modal-header h5.modal-title').html($(this).text())
+
+      let myModal = new bootstrap.Modal($('#modalId'))
+
+      myModal.show()
 
 
+   })
+
+   /* campo obrigatorio com Botão Submit(enviar) 
+   
+   $('body').on('submit', '.modal-body .form', function(e){
+
+      e.preventDefault()
+
+      const inputName = $('#nome')
+      const inputEmail = $('#email')
+
+      if( inputName.val() == '') {
+
+         console.log('o campo de nome é obrigatório')
+         
+         inputName.addClass('invalid')
+
+         return false
+      }
+
+      if( inputEmail.val() == '') {
+
+         console.log('o campo de e-mail é obrigatório')
+         
+         inputEmail.addClass('invalid')
+
+         return false
+      }
+   })
+   */
+
+   /* Campo obrigatório perdendo o foco com "blur' */
+
+   function validate( elem ){
+      if( elem.val() == '') {
+      
+         console.log('o campo de '+ elem.attr('name') + ' é obrigatório')
+
+         elem.parent().find('.text-muted').show()
+   
+         elem.addClass('invalid')
+   
+         return false
+      } else {
+         elem.parent().find('.text-muted').hide()
+         elem.removeClass('invalid')
+      }
+      
+   }
+
+   $('body').on('submit', '.modal-body .form', function(e){
+
+      e.preventDefault()
+
+      const inputName = $('#nome')
+      const inputEmail = $('#email')
+
+      validate(inputName)
+      validate(inputEmail)
+
+      if(inputEmail.hasClass('invalid')  || inputName.hasClass('invalid')){
+         console.log('verificar campos obrigatórios')
+         return false
+      } else {
+         $(this).submit()
+      }
+
+   })
 
 
+   
+   $('body').on('blur', '#nome', function(){
+     validate($(this))
+   })
 
+   $('body').on('blur', '#email', function(){
+     validate($(this))
+   })
+
+   $('body').on('focus', '#date', function(){
+      $(this).datepicker()
+   })
+
+   $('body').on('blur', '#date', function(){
+      validate($(this))
+      $(this).mask('00/00/0000');
+   }) 
+   
+   $('body').on('blur', '#time', function(){
+      validate($(this))
+      $(this).mask('00:00');
+   })
+
+   $('body').on('blur', '#cep', function(){
+      validate($(this))
+      $(this).mask('00000-000');
+   })
+   
+   
+   $('body').on('blur', '#phone_with_ddd', function(){
+      validate($(this))
+      $(this).mask('(00) 00000-0000');
+   })
+   
+   $('body').on('blur', '#cpf', function(){
+      validate($(this))
+      $(this).mask('000.000.000-00');
+   })
+   
+   
 
 
 })
